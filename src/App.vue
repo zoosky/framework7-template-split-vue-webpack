63c62cfc564cc80aab85a96ad9f85eaa7262f985
ZooSky <zoosky@gmail.com>
2018-06-01 10:11:48 +0200

    pop-up

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,29 @@
   <div id="app">
     <!-- Statusbar -->
     <f7-statusbar></f7-statusbar>
+    <panel-left></panel-left>
+    <panel-right></panel-right>
           <!-- Main view-->
     <f7-view main ios-edges>
+      
       <home></home>
     </f7-view>
+    <pop-up></pop-up>
   </div>
 </template>
 
 <script>
 import Home from "./pages/home.vue";
-
+import PanelLeft from "./pages/panel-left.vue";
+import PanelRight from "./pages/panel-right.vue";
+import PopUp from "./components/popup.vue";
 export default {
   name: "app",
   components: {
-    Home
+    Home,
+    PanelLeft,
+    PanelRight,
+    PopUp
   }
 };
 </script>
---
+++ src/components/popup.vue
@@ -0,0 +1,15 @@
+<template>
+  <!-- Popup -->
+  <f7-popup id="popup">
+    <f7-view>
+      <f7-page>
+        <f7-navbar title="Popup">
+          <f7-nav-right>
+            <f7-link popup-close>Close</f7-link>
+          </f7-nav-right>
+        </f7-navbar>
+        <f7-block>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, architecto. Cupiditate laudantium rem nesciunt numquam, ipsam. Voluptates omnis, a inventore atque ratione aliquam. Omnis iusto nemo quos ullam obcaecati, quod.</f7-block>
+      </f7-page>
+    </f7-view>
+  </f7-popup>
+</template>
