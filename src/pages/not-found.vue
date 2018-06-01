c44cc0915ec12fafc1e928bbdbd898cb870552b7
ZooSky <zoosky@gmail.com>
2018-06-01 19:19:21 +0200

    not found

---
+++ src/pages/not-found.vue
@@ -0,0 +1,13 @@
+<template>
+  <f7-page>
+    <f7-navbar title="Not found" back-link="Back"></f7-navbar>
+    <f7-block strong>
+      <p>Sorry</p>
+      <p>Requested content not found.</p>
+      <p>You can go <f7-link back>back</f7-link>.</p>
+    </f7-block>
+  </f7-page>
+</template>
+<script>
+export default {};
+</script>
