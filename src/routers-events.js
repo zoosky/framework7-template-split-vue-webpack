304f223b585202287c52764f174677e53ffab31e
ZooSky <zoosky@gmail.com>
2018-06-02 10:13:12 +0200

    less output

--- src/routers-events.js
+++ src/routers-events.js
@@ -1,10 +1,10 @@
 export default {
   pageAfterIn: (e, page) => {
     // do something after page gets into the view
-    console.log("pageAfterIn: ", page.route.url, page, e);
+    console.log("pageAfterIn: ", page.route.url, e.type, page);
   },
   pageInit: (e, page) => {
     // do something when page initialized
-    console.log("pageInit: ", page.route.url, page, e);
+    console.log("pageInit: ", page.route.url, e.type, page);
   }
 };
