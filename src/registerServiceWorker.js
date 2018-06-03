50305f97d7ffa2ad93fc14b232f3cd80fb9020a0
ZooSky <zoosky@gmail.com>
2018-06-03 21:38:37 +0200

    sw comments

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -6,23 +6,23 @@ if (process.env.NODE_ENV === "production") {
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready() {
       console.log(
-        "App is being served from cache by a service worker.\n" +
+        "SW: App is being served from cache by a service worker.\n" +
           "For more details, visit https://goo.gl/AFskqB"
       );
     },
     cached() {
-      console.log("Content has been cached for offline use.");
+      console.log("SW: Content has been cached for offline use.");
     },
     updated() {
-      console.log("New content is available; please refresh.");
+      console.log("SW: New content is available; please refresh.");
     },
     offline() {
       console.log(
-        "No internet connection found. App is running in offline mode."
+        "SW: No internet connection found. App is running in offline mode."
       );
     },
     error(error) {
-      console.error("Error during service worker registration:", error);
+      console.error("SW: Error during service worker registration:", error);
     }
   });
 }
