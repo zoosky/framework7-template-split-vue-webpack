34c1d4d9d72bc4df7d5093187ac5ea8227f1ea48
ZooSky <zoosky@gmail.com>
2018-06-12 21:28:37 +0200

    toogle toolbar position

--- src/pages/tabs-routable.vue
+++ src/pages/tabs-routable.vue
@@ -1,6 +1,12 @@
 <template>
   <f7-page :page-content="false">
-    <f7-navbar title="Tabs Routable" back-link="Back"></f7-navbar>
+    <f7-navbar title="Tabs Routable" back-link="Back">
+       <f7-nav-right v-if="$theme.md">
+        <f7-link icon="material:compare_arrows" @click="toggleToolbarPosition">
+          <i class="icon material-icons rotate-icon">compare_arrows</i>
+        </f7-link>
+      </f7-nav-right>
+    </f7-navbar>
     <f7-toolbar tabbar>
       <f7-link tab-link href="./" route-tab-id="tab1">Tab 1</f7-link>
       <f7-link tab-link href="tab2/" route-tab-id="tab2">Tab 2</f7-link>
@@ -33,6 +39,13 @@ export default {
     f7Tab,
     f7Link,
     f7Toolbar
+  },
+  methods: {
+    toggleToolbarPosition() {
+      this.$$(this.$el)
+        .find(".toolbar, .tabbar")
+        .toggleClass("toolbar-bottom-md");
+    }
   }
 };
 </script>
