458b188beb9b6d6e6524d1c706ae15bb833d97f1
ZooSky <zoosky@gmail.com>
2018-07-09 23:26:51 +0200

    changes wip

--- src/main.js
+++ src/main.js
@@ -4,14 +4,14 @@ import store from "./store";
 import "./registerServiceWorker";
 
 // Import F7
-import Framework7 from "framework7/dist/framework7.esm.bundle.js";
+import Framework7 from "framework7/framework7.esm.bundle.js";
 
 // Import F7 Vue Plugin
-import Framework7Vue from "framework7-vue/dist/framework7-vue.esm.bundle.js";
+import Framework7Vue from "framework7-vue/framework7-vue.esm.bundle.js";
 
 // Import F7 Styles
 // eslint-disable-next-line
-import Framework7Styles from "framework7/dist/css/framework7.css";
+import Framework7Styles from "framework7/css/framework7.css";
 
 // Import Icons and App Custom Styles
 // eslint-disable-next-line
@@ -19,33 +19,14 @@ import IconsStyles from "./assets/css/icons.css";
 // eslint-disable-next-line
 import AppStyles from "./assets/css/app.css";
 
-// Import Routes
-import Routes from "./routes.js";
-
 // Init F7 Vue Plugin
-Vue.use(Framework7Vue, Framework7);
+Framework7.use(Framework7Vue);
 
 Vue.config.productionTip = false;
 
 new Vue({
   // Init Framework7 by passing parameters here
-  framework7: {
-    id: "io.framework7.testapp", // App bundle ID
-    name: "Framework7 Split-View with Vuejs Compnents", // App name
-    theme: "auto", // Automatic theme detection
-    // App routes
-    routes: Routes,
-    // Enable panel left visibility breakpoint
-    panel: {
-      leftBreakpoint: 960
-    },
-    view: {
-      // configuration examples
-      iosDynamicNavbar: true, //default: true
-      xhrCache: true, //default: true
-      pushState: true //default: false
-    }
-  },
+
   store,
   methods: {
     onF7Ready(f7) {
